f4d0177e23d64600eea5886ed4f11fa97598fa1f
gabibaidan <55233729+gabibaidan@users.noreply.github.com>
2019-09-12 17:41:09 +0300

    part 2

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # camera-app
-web browser camera app (for IOS)
+Super awesome camera web app built with HTML, CSS, and JS.
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "environment" }, audio: false };
+var constraints = { video: { facingMode: "user" }, audio: false };
 var track = null;
 
 // Define constants
@@ -33,3 +33,17 @@ cameraTrigger.onclick = function() {
 
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
+
+
+// Install ServiceWorker
+if ('serviceWorker' in navigator) {
+  console.log('CLIENT: service worker registration in progress.');
+  navigator.serviceWorker.register( '/camera-app/part-2/sw.js' , { scope : ' ' } ).then(function() {
+    console.log('CLIENT: service worker registration complete.');
+  }, function() {
+    console.log('CLIENT: service worker registration failure.');
+  });
+} else {
+  console.log('CLIENT: service worker is not supported.');
+}
+
